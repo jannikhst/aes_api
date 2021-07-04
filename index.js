7ff2b7bebde4ddf762d42cfdfeb1598992072590
Jannik <ge93tev@mytum.de>
2021-07-04 16:11:36 +0200

    changed console log position

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ app.use(router);
 router.post('/aes-ecb/encrypt', (req, res, next) => {
     const key = req.body['key'];
     const content = req.body['content'];
-    const encrypted = aes.encrypt(key, content);
-    const json = {};
     console.log(key);
     console.log(content);
+    const encrypted = aes.encrypt(key, content);
+    const json = {};
+
     json.message = 'Success';
     json.result = encrypted;
     res.send(JSON.stringify(json));
@@ -30,10 +31,11 @@ router.get('/check', (req, res, next) => {
 router.post('/aes-ecb/decrypt', (req, res, next) => {
     const key = req.body['key'];
     const content = req.body['content'];
-    const decrypted = aes.decrypt(key, content);
-    const json = {};
     console.log(key);
     console.log(content);
+    const decrypted = aes.decrypt(key, content);
+    const json = {};
+
     json.message = 'Success';
     json.result = decrypted;
     res.send(JSON.stringify(json));
